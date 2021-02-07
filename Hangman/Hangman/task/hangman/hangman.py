82060fd553fdcfe7601deea3447428efeb1d86f4
Jebadeiah <michael.nevin.miller@gmail.com>
2021-02-06 16:18:06 -0800

    Provided the user with a hint that shows the first three characters of the word and dashes covering the remaining characters.
    Signed-off-by: Jebadeiah <michael.nevin.miller@gmail.com>

--- Hangman/Hangman/task/hangman/hangman.py
+++ Hangman/Hangman/task/hangman/hangman.py
@@ -3,10 +3,15 @@ import random
 
 words = ['python', 'java', 'kotlin', 'javascript']
 word = words[random.randint(0, 3)]
+hint = ""
 
+for i in word[0:3]:
+    hint += i
+
+hint += ('-' * (len(word) - 3))
 
 print("H A N G M A N")
-guess = input("Guess the word: ")
+guess = input("Guess the word {}: ".format(hint))
 if guess == word:
     print("You survived!")
 else:
