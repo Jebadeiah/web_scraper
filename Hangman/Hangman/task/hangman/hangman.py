0cc5151f428dd860a2d7f89ce7ad68658d85de11
Jebadeiah <michael.nevin.miller@gmail.com>
2021-02-06 16:56:37 -0800

    I started making it into what looks like a game, but I'm trying to figure out how to replace the dashes that indicate letters with the letters they're supposed to be. Somehow I need to log the indexes for the guess letter in word and then replace every dash in hint that correlates to a letter matching the guess in word.
    Signed-off-by: Jebadeiah <michael.nevin.miller@gmail.com>

--- Hangman/Hangman/task/hangman/hangman.py
+++ Hangman/Hangman/task/hangman/hangman.py
@@ -3,16 +3,19 @@ import random
 
 words = ['python', 'java', 'kotlin', 'javascript']
 word = words[random.randint(0, 3)]
-hint = ""
+hint = ('-' * (len(word)))
+tries = 8
 
-for i in word[0:3]:
-    hint += i
-
-hint += ('-' * (len(word) - 3))
 
 print("H A N G M A N")
-guess = input("Guess the word {}: ".format(hint))
-if guess == word:
-    print("You survived!")
-else:
-    print("You lost!")
+while tries > 0:
+    print(hint)
+    guess = input("Input a letter")
+    if guess in word:
+        word.replace()
+        print("You survived!")
+    else:
+        tries -= 1
+        print("Whoops! Only {} tries left!".format(tries))
+
+print("You're trash! You're garbage!")
