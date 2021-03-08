69b4ad0d4358aa235a5603bd26a67866b59240b7
Jebadeiah <michael.nevin.miller@gmail.com>
2021-03-07 20:34:14 -0800

    I've gotten the code nearly to completion for this step, but it's not passing. I know that the inline code function isn't working, but I'm trying to test the rest of the code, first, and it's not passing either. Calling it a day for now though.

--- Markdown Editor/Markdown Editor/task/editor.py
+++ Markdown Editor/Markdown Editor/task/editor.py
@@ -1,10 +1,86 @@
 formatters = ("plain", "bold", "italic", "link",
               "inline-code", "header", "ordered-list", "unordered-list", "line-break")
 
+
+def plain(doc, text):
+    doc += f"{text}"
+    return doc
+
+
+def bold(doc, text):
+    doc += f"**{text}**"
+    return doc
+
+
+def italic(doc, text):
+    doc += f"*{text}*"
+    return doc
+
+
+def link(doc, label, text):
+    doc += f"[{label}]({text})"
+    return doc
+
+
+def inline_code(doc, text):
+    pass
+
+
+def header(doc, level, text):
+    doc += f"{'#' * level} {text}\n"
+    return doc
+
+
+def ordered_list(doc, text):
+    pass
+
+
+def unordered_list(doc, text):
+    pass
+
+
+def line_break(doc):
+    doc += '\n'
+    return doc
+
+
 while True:
+    document = ""
     user_input = input("- Choose a formatter:")
     if user_input in formatters:
-        continue
+        if user_input.lower() == "plain":
+            document = plain(document, input("- Text: "))
+            print(document)
+
+        elif user_input.lower() == "bold":
+            document = bold(document, input("- Text: "))
+            print(document)
+
+        elif user_input.lower() == "italic":
+            document = italic(document, input("- Text: "))
+            print(document)
+
+        elif user_input.lower() == "link":
+            document = link(document, input("- Label: "), input("- URL: "))
+            print(document)
+
+        elif user_input.lower() == "inline-code":
+            pass
+
+        elif user_input.lower() == "header":
+            document = header(document, int(input("- Level: ")), input("- Text: "))
+            print(document)
+
+        elif user_input.lower() == "ordered list":
+            pass
+
+        elif user_input.lower() == "unordered list":
+            pass
+
+        elif user_input.lower() == "new-line":
+            document = line_break(document)
+            print(document)
+
     elif user_input == "!help":
         print(f'''Available formatters: {" ".join(formatters)}
         Special commands: !help !done''')
