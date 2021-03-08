7f56702fd02729d731550a1ec8330ffc86281801
Jebadeiah <38235842+Jebadeiah@users.noreply.github.com>
2021-03-07 19:12:14 -0800

    Delete Coffee Machine directory

--- Coffee Machine/Problems/Task/main.py
+++
@@ -1,9 +0,0 @@
-class Task:
-    def __init__(self, description, team):
-        self.description = description
-        self.team = team
-
-    def __add__(self, other):
-        descrip = f"{self.description}\n{other.description}"
-        tm = f"{self.team}, {other.team}"
-        return Task(descrip, tm)
