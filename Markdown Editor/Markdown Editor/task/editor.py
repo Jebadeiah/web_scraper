124dd464aef4bd841a528bc6398732fcc92a6cbe
Jebadeiah <michael.nevin.miller@gmail.com>
2021-03-07 19:38:49 -0800

    This second section of the project made more sense. I set up a loop that prompted the user for input and set up !help and !done commands.

--- Markdown Editor/Markdown Editor/task/editor.py
+++ Markdown Editor/Markdown Editor/task/editor.py
@@ -1,8 +1,14 @@
-print('''# John Lennon
-or ***John Winston Ono Lennon*** was one of *The Beatles*.
-Here are the songs he wrote I like the most:
-* Imagine
-* Norwegian Wood
-* Come Together
-* In My Life
-* ~~Hey Jude~~ (that was *McCartney*)''')
+formatters = ("plain", "bold", "italic", "link",
+              "inline-code", "header", "ordered-list", "unordered-list", "line-break")
+
+while True:
+    user_input = input("- Choose a formatter:")
+    if user_input in formatters:
+        continue
+    elif user_input == "!help":
+        print(f'''Available formatters: {" ".join(formatters)}
+        Special commands: !help !done''')
+    elif user_input == "!done":
+        break
+    else:
+        print("Unknown formatting type or command")
